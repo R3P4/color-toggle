9a70676378d73bdaef7d9dec1cbe8c1c8312a6e0
R3P4 <104983126+R3P4@users.noreply.github.com>
2022-09-26 14:52:34 +0200

    update js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const klick = function() {
-    const white = document.querySelector('.white-background');
+const click = function() {
+    const white = document.querySelector(body);
     body.classList.toggle('.white-background');
-    const red = document.querySelector('.red-background');
+    const red = document.querySelector(body);
     body.classList.toggle('.red-background');
-    const orange = document.querySelector('.orange-background');
+    const orange = document.querySelector(body);
     body.classList.toggle('.orange-background');
-    const purple = document.querySelector('.purple-background');
+    const purple = document.querySelector(body);
     body.classList.toggle('.purple-background');
-    const green = document.querySelector('.green-background');
+    const green = document.querySelector(body);
     body.classList.toggle('.green-background');
     console.log();
 };
-const body = document.querySelectorAll('.white-background' , '.red-background' , '.orange-background' , '.purple-background' , '.green-background');
-radio.addEventListener('click', klick);
-
-
+const white = document.querySelector('.white-background');
+radio.addEventListener('click', ()=>{body.style.background="white"});
+const red = document.querySelector( '.red-background');
+radio.addEventListener('click', ()=>{body.style.background="red"});
+const orange = document.querySelector('.orange-background');
+radio.addEventListener('click', ()=>{body.style.background="orange"});
+const purple = document.querySelector('.purple-background');
+radio.addEventListener('click', ()=>{body.style.background="purple"});
+const green = document.querySelector('.green-background');
+radio.addEventListener('click', ()=>{body.style.background="green"});
 
 /*
 const click = function(){
